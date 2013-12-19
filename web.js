fe86936ed9bf15a4087b8c1e498e17abb298dc56
Omer  Korner <omerkorner@gmail.com>
2013-12-19 16:12:07 +0000

    Added file path solution

--- web.js
+++ web.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var fs = require('fs');
+var path = require('path');
 var app = express();
 app.use(express.logger());
 
+var filePath = path.join(__dirname + '/index.html');
+
 app.get('/', function(request, response) {
-  var file = fs.readFile('index.html');
+  var file = fs.readFile(filePath);
   var fileContents = file.toString();
   response.send(fileContents);
 });
